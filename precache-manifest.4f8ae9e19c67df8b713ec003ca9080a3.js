b646a20b74814acdd350bca2e03fa9b8c4983fb3
arbaazshaikh <abzshaikh@gmail.com>
2020-09-28 01:03:10 +0530

    Updates

---
+++ precache-manifest.4f8ae9e19c67df8b713ec003ca9080a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573d9dbf228edc49a9583698c12a2b04",
+    "url": "/testproject-1/index.html"
+  },
+  {
+    "revision": "8213f114bcf4dc6ab5ab",
+    "url": "/testproject-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f742609718a1a8811a89",
+    "url": "/testproject-1/static/js/2.27a9e802.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testproject-1/static/js/2.27a9e802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8213f114bcf4dc6ab5ab",
+    "url": "/testproject-1/static/js/main.c6d544f2.chunk.js"
+  },
+  {
+    "revision": "e1397bc0e203d71bc313",
+    "url": "/testproject-1/static/js/runtime-main.0f3e2ed5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testproject-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
