b136c676ddf97cc0feef6daff82f6f4920077a31
arbaazshaikh <abzshaikh@gmail.com>
2020-09-28 01:48:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './css/main.css'
 
 function App() {
   return (
     <div>
       <h1>Hello world!!!</h1>
       <h2>I am awesome!!!</h2>
+      <h3>This is a new project</h3>
     </div>
   );
 }
